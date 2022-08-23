7551bb438f4558498ec76718320a1a55fba35df9
nirbhay <ssn403585@gmail.com>
2022-08-23 11:37:18 +0530

    delete the file of frontend

--- backend/Utils/checkAuth.js
+++ backend/Utils/checkAuth.js
@@ -9,7 +9,7 @@ module.exports = (context) => {
         const token = authHeader.split("Bearer ")[1];
         if(token){
             try{
-                 const user = jwt.verify(token, "superSecretPassword");
+                 const user =jwt.verify(token, "superSecretPassword");
                  return user;
             }catch(err){
                 throw new AuthenticationError("Invalid/Expired token")
